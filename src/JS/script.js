773b5cc318a388dedf9ccf5d6be30cd6e61b4937
Sarthak-Lolge <lolgesarthak@gmail.com>
2025-02-20 22:15:33 +0530

    Adding About Us page

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,10 +1,15 @@
 let slideIndex = 0;
-        const slides = document.querySelector('.slides');
-        const totalSlides = document.querySelectorAll('.slide').length;
+const slides = document.querySelector(".slides");
+const totalSlides = document.querySelectorAll(".slide").length;
 
-        function moveSlide(step) {
-            slideIndex += step;
-            if (slideIndex >= totalSlides) slideIndex = 0;
-            if (slideIndex < 0) slideIndex = totalSlides - 1;
-            slides.style.transform = `translateX(${-slideIndex * 100}%)`;
-        }
+function moveSlide(step) {
+  slideIndex += step;
+  if (slideIndex >= totalSlides) slideIndex = 0;
+  if (slideIndex < 0) slideIndex = totalSlides - 1;
+  slides.style.transform = `translateX(${-slideIndex * 100}%)`;
+}
+
+// about us
+function toggleAnswer(element) {
+  element.parentNode.classList.toggle("open");
+}
